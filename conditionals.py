5279f7cb6c9273de7b23b43f30932b47192a9b97
Mariana Cardoso <marianacardoso.tds2.senai@gmail.com>
2024-10-21 20:05:07 -0300

    feat: create conditionals.py file

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+# Solicita ao usuário que insira um número
+numero = float(input("Digite um número: 60"))
+
+# Verifica se o número é positivo, negativo ou zero
+if numero > 0: # Se o número for maior que zero
+    print("O número é positivo.")
+elif numero < 0: # Se o número for menor que zero
+    print("O número é negativo.")
+else:
+    print("O número é zero.")  # Se o número for igual a zero
